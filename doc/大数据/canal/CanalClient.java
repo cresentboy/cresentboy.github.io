86cbc82cdcc3957830a4216b2889e7777717d923
zhigang <fdrgdf@foxmail.com>
2022-02-27 20:47:23 +0800

    canal client

---
+++ doc/大数据/canal/CanalClient.java
@@ -0,0 +1,93 @@
+package com.atguigu;
+
+import com.alibaba.fastjson.JSONObject;
+import com.alibaba.otter.canal.client.CanalConnector;
+import com.alibaba.otter.canal.client.CanalConnectors;
+import com.alibaba.otter.canal.protocol.CanalEntry;
+import com.alibaba.otter.canal.protocol.Message;
+import com.google.protobuf.ByteString;
+import com.google.protobuf.InvalidProtocolBufferException;
+
+import java.net.InetSocketAddress;
+import java.util.List;
+
+public class CanalClient {
+
+    public static void main(String[] args) throws InterruptedException, InvalidProtocolBufferException {
+
+        //TODO 获取连接
+        CanalConnector canalConnector = CanalConnectors.newSingleConnector(new InetSocketAddress("hadoop102", 11111), "example", "", "");
+
+        while (true) {
+
+            //TODO 连接
+            canalConnector.connect();
+
+            //TODO 订阅数据库
+            canalConnector.subscribe("gmall-2021.*");
+
+            //TODO 获取数据
+            Message message = canalConnector.get(100);
+
+            //TODO 获取Entry集合
+            List<CanalEntry.Entry> entries = message.getEntries();
+
+            //TODO 判断集合是否为空,如果为空,则等待一会继续拉取数据
+            if (entries.size() <= 0) {
+                System.out.println("当次抓取没有数据，休息一会。。。。。。");
+                Thread.sleep(1000);
+            } else {
+
+                //TODO 遍历entries，单条解析
+                for (CanalEntry.Entry entry : entries) {
+
+                    //1.获取表名
+                    String tableName = entry.getHeader().getTableName();
+
+                    //2.获取类型
+                    CanalEntry.EntryType entryType = entry.getEntryType();
+
+                    //3.获取序列化后的数据
+                    ByteString storeValue = entry.getStoreValue();
+
+                    //4.判断当前entryType类型是否为ROWDATA
+                    if (CanalEntry.EntryType.ROWDATA.equals(entryType)) {
+
+                        //5.反序列化数据
+                        CanalEntry.RowChange rowChange = CanalEntry.RowChange.parseFrom(storeValue);
+
+                        //6.获取当前事件的操作类型
+                        CanalEntry.EventType eventType = rowChange.getEventType();
+
+                        //7.获取数据集
+                        List<CanalEntry.RowData> rowDataList = rowChange.getRowDatasList();
+
+                        //8.遍历rowDataList，并打印数据集
+                        for (CanalEntry.RowData rowData : rowDataList) {
+
+                            JSONObject beforeData = new JSONObject();
+                            List<CanalEntry.Column> beforeColumnsList = rowData.getBeforeColumnsList();
+                            for (CanalEntry.Column column : beforeColumnsList) {
+                                beforeData.put(column.getName(), column.getValue());
+                            }
+
+                            JSONObject afterData = new JSONObject();
+                            List<CanalEntry.Column> afterColumnsList = rowData.getAfterColumnsList();
+                            for (CanalEntry.Column column : afterColumnsList) {
+                                afterData.put(column.getName(), column.getValue());
+                            }
+
+                            //数据打印
+                            System.out.println("Table:" + tableName +
+                                    ",EventType:" + eventType +
+                                    ",Before:" + beforeData +
+                                    ",After:" + afterData);
+                        }
+                    } else {
+                        System.out.println("当前操作类型为：" + entryType);
+                    }
+                }
+            }
+        }
+    }
+}
